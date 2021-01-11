8dea59d52d4a55a43523a578c9ef2e853695d962
HelenaIsh <elena-ismatova@mail.ru>
2021-01-11 16:05:57 +0500

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let rules = [];
+    let result = str;
+    for (let [openBracket, closeBracket] of bracketsConfig) {
+        let rule = `${openBracket}${closeBracket}`
+        rules.push(rule);
+    }
+    while (true) {
+        if (result === '') return true;
+        let foundRule = findRule(result, rules);
+        if (foundRule) {
+            result = result.replace(foundRule,'')
+        } else return false;
+    }
+}
+function findRule(result, rules){
+    for (let rule of rules) {
+        if (result.includes(rule)) {
+            return rule;
+        }
+    }
 }
